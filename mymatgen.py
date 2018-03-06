e8b452c523cd64a412dce0e93dff9c60ca67377f
Mitchell Koerner <mfkoerner@gmail.com>
2018-03-05 17:07:50 -0800

    mymatgen: download_cifs added style options

--- mymatgen.py
+++ mymatgen.py
@@ -87,21 +87,22 @@ def vol_from_abc(a, b, c, Ain, Bin, Cin):
     V = np.sqrt(1 - np.cos(A)**2 - np.cos(B)**2 - np.cos(C)**2 + 2*np.cos(A)*np.cos(B)*np.cos(C)) * a * b * c
     return(V)
 
-def download_cifs(mpids, zipfilename):
+def download_cifs(mpids, zipfilename='cifs', style='cifs.computed'):
     """deposits cif files from materials project into a zipped directory
 
     Inputs
         mpids: iterable of materials id strings such as 'mp-19' to get POSCARS for
         zipfilename: filename of zip folder created
+        style: choose which cif to get from materialsproject
 
     Outputs
-        places POSCAR files into filepath/{mpid}_POSCAR
+        places cif files into filepath/zipfilename
     """
     with MPRester(config.matprojapi) as mpr:
         docs = mpr.query({'material_id': {'$in': mpids}}, ['material_id', 'pretty_formula','cifs.computed'])
     with ZipFile(zipfilename, 'w') as f:
         for d in docs:
-            f.writestr('{}.cif'.format(d['material_id']),d['cifs.computed'])
+            f.writestr('{}.cif'.format(d['material_id']),d[style])
 
 
 # def fullprint(*args, **kwargs):
